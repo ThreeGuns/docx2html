d9d0bc4409beb1a6cc2054dabde83ceaf99358f0
cailong1028 <cailong1028@126.com>
2018-03-19 16:56:54 +0800

    修改inline style rFonts 参数为null时候的bug

--- test/testImg.js
+++ test/testImg.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 
 const docsDirPath = './docs';
 const htmlDirPath = './html';
+const htmlFileSuffix = '.html';
 const imgDocFilePath = '图片doc.docx';
 
 const imgDocFileFullPath = path.resolve(__dirname, docsDirPath, imgDocFilePath);
 docx2html(imgDocFileFullPath).then(function(html){
     console.log(html.toString());
-    const generateFilePath = path.resolve(htmlDirPath, path.basePath(imgDocFileFullPath));
+    const generateFilePath = path.resolve(htmlDirPath, path.basename(imgDocFileFullPath)+htmlFileSuffix);
     fs.writeFile(generateFilePath, html, {encoding: 'utf8'}, (err) => {
         if(err){
             return console.error(err);
