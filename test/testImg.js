c60625b398df5441715226bf1db91849e1e3f03a
cailong1028 <cailong1028@126.com>
2018-03-19 16:46:15 +0800

    添加测试

---
+++ test/testImg.js
@@ -0,0 +1,21 @@
+const docx2html = require('../lib/index');
+const path = require('path');
+const fs = require('fs');
+
+const docsDirPath = './docs';
+const htmlDirPath = './html';
+const imgDocFilePath = '图片doc.docx';
+
+const imgDocFileFullPath = path.resolve(__dirname, docsDirPath, imgDocFilePath);
+docx2html(imgDocFileFullPath).then(function(html){
+    console.log(html.toString());
+    const generateFilePath = path.resolve(htmlDirPath, path.basePath(imgDocFileFullPath));
+    fs.writeFile(generateFilePath, html, {encoding: 'utf8'}, (err) => {
+        if(err){
+            return console.error(err);
+        }
+        console.log(`generate file [${generateFilePath}] done`);
+    });
+}).catch(e => {
+    console.error(e);
+});
